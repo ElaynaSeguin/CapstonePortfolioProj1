585c4337bfee20567ae2e18ab4534dcaf85f2650
ldo7 <do.lani.do@gmail.com>
2023-10-25 21:49:50 -0700

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include <fstream>
 #include <bitset>
 #include <ctime>
+#include <vector>
 #include <unistd.h>
-// #include "cpu.h"
+#include "cpu.h"
 using namespace std;
 
 void readFile();
@@ -14,25 +15,30 @@ void clockStart();
 void clockStop();
 clock_t timer;
 int totalTime = 0;
-// CPU cpu;
+uint32_t breakpoints[5] = {0xFFFFFFFF}; // initialize to FF (never reached)
+// cpu CPU;
 
 int main()
 {
-    readFile();
+    // readFile();
 
     // INTANTIATE & RUN CPU
 
-    //REQUEST USER INPUT
-    userInput();  
+    // REQUEST USER INPUT
+    while (true)
+    {
+        userInput();
+    }
+    
 
-    //DISPLAY TIME
-    cout << "\nTotal Time:" << totalTime;
-    clockStart();
-    sleep(10);
-    clockStop();
-    cout << fixed << "\nTotal Time:" << (float)totalTime / CLOCKS_PER_SEC << " sec" << scientific;
+    // DISPLAY TIME
+    //  cout << "\nTotal Time:" << totalTime;
+    //  clockStart();
+    //  sleep(10);
+    //  clockStop();
+    //  cout << fixed << "\nTotal Time:" << (float)totalTime / CLOCKS_PER_SEC << " sec" << scientific;
 
-    writeFile();
+    // writeFile();
 
     return 0;
 }
@@ -43,7 +49,7 @@ void readFile()
     // OPEN file
     string line, input = "input.dat";
     ifstream file(input);
-    vector<uint32_t> arr;
+    vector<uint32_t> list;
     if (!file)
     {
         cout << "ERROR. Cannot open file";
@@ -52,16 +58,36 @@ void readFile()
     cout << "\n~~ Loading instructions ~~\n";
 
     // READ file
+    bitset<32> combinedBinary(0); // intialize 32-bit value = 0000 0000 0000 0000
+    string combinedString;
     while (getline(file, line))
     {
         uint32_t val = bitset<32>(line).to_ulong(); // Convert the binary string to uint32_t
-        arr.push_back(val);
-        // cout << val << "\n";         // 255       int
-        cout << bitset<8>(val) << "\n"; // 11111111  binary
 
-        //read every 4 lines = 1 instruction
+        // every 4 lines = 1 instruction
+        combinedString = line + combinedString; 
+        if (combinedString.length() == 32)      //append line until 32-bits
+        {
+            // convert string to binary
+            for (int i = 0; i < 32; i++)
+            {
+                if (combinedString[i] == '1')
+                    combinedBinary.set(i);
+                else
+                    combinedBinary.reset(i);
+            }
+            // insert the value into the vector
+            list.push_back(combinedBinary.to_ulong());
+            // cout << combinedBinary<<"\n";  //DEBUG, check little-endian order
+            combinedBinary.reset();
+            combinedString.clear();
+        }
     }
     file.close();
+
+    // for(bitset<32> instr:list){  //DEBUG
+    //     cout << instr << "\n";
+    // }
 
     cout << "~~ Loading complete ~~\n";
 }
@@ -70,7 +96,7 @@ void readFile()
 void writeFile()
 {
     string output = "output.asm";
-    ofstream file(output,ios::app); //append
+    ofstream file(output, ios::app); // append
     if (!file)
     {
         cout << "ERROR. Cannot open file";
@@ -93,51 +119,86 @@ void clockStop()
     totalTime += timer;
 }
 
-void displayOptions(){
-    cout  << "\n" << "Enter a command: \n"
-          << "\n" << "r     - run entire program"
-          << "\n" << "s     - run next instruction"
-          << "\n" << "x0    - view content in this register"
-          << "\n" << "0x12345678 - view content at this address"
-          << "\n" << "pc    - view PC value"
-          << "\n" << "insn  - view next instruction"
-          << "\n" << "b[pc] - set breakpoint"
-          << "\n" << "c     - continue execution";
+void displayOptions()
+{
+    cout << "\n" << "r     - run entire program"
+         << "\n" << "s     - run next instruction"
+         << "\n" << "x0    - view content in this register"
+         << "\n" << "0x12345678 - view content at this address"
+         << "\n" << "pc    - viewb  PC value"
+         << "\n" << "insn  - view next instruction"
+         << "\n" << "b[pc] - set breakpoint"
+         << "\n" << "c     - continue execution";
 }
 
-void userInput(){
-    string input=" ";
+void userInput()
+{
+    string input = " ";
     displayOptions();
-    // getline(cin,input);
+    cout << "\n\nEnter a command: ";
+    getline(cin,input);
+    
+    // Remove all whitespaces from input
+    input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
+    string c = input.substr(0,1);
+    int num;
 
-//     //convert input to option type
+    //VALIDATE INPUT
+    // run - execute all
+    // c - continue 
+    if (c=="r" || c=="c"){
+        // clockStart();
 
-//     switch(input){
-//         case "r":
+    }
+    // s - step through
+    else if (c=="s"){
 
-//             break;
-//         case "s":
-
-//             break;
-//         case "pc":
-
-//             break;
-//         case "insn":
-
-//             break;
-//         case "c":
-
-//             break;
-//         case "x0":
-
-//             break;
-//         case "0x12345678":
-
-//             break;
-//         case "b [pc]":
-
-//             break;
-//         default:
-//             cout <<"Invalid input";
-//     }
+    }
+    // x0 - x31
+    else if (c=="x"){
+        c=input.substr(1,2);
+        num = stoi(c);
+        if(num > 31)
+            cout << "Invalid input\n";
+        // else{
+        //     cout << CPU.getReg(num);
+        // }
+    }
+    // 0x12345678 - return val at address
+    else if (c=="0"){
+        c=input.substr(2,8);
+        num = stoi(c);
+        // cout << CPU.getMem(num);
+    }
+    // pc - return PC 
+    else if (c=="p"){
+        if(input!="pc")            
+            cout << "Invalid input\n";
+        // else{
+        //     cout << "0x"<<CPU.getPC();
+        // }
+    }
+    // insn - return NEXT asm instruction
+    else if (c=="i"){
+        if(input!="insn")            
+            cout << "Invalid input\n";
+        // else{
+        //     cout << CPU.debug();
+        // }
+    }
+    // b[pc] - add breakpoint     assume format -> b[0x12345678]
+    else if (c=="b"){
+        c=input.substr(4,8);
+        num = stoi(c);
+        //ASSUME SORTED IN-ORDER....
+        for(uint32_t i:breakpoints){   //check is breakpoints[] is full 
+            if(i!=0xFFFFFFFF){
+                i = num;
+                break;
+            }
+        }
+    }
+    else {
+        cout << "Invalid input\n";
+    }
 }
